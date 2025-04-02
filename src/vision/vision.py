7f6a4d68ac9fb2d9aca131a1901e87e523f5ed7d
Nick Burczyk <nburczyk@icloud.com>
2025-04-02 12:27:33 -0500

    Slightly sped up depth_estimation(), also starting to add documentation to Vision class

--- src/vision/vision.py
+++ src/vision/vision.py
@@ -29,6 +29,12 @@ class Vision:
         self.cam_matrix = None
         self.dist_coeffs = None
 
+        self.map1_x = None
+        self.map1_y = None
+        self.map2_x = None
+        self.map2_y = None
+        self.Q = None
+
         self.highlight_min_dist = self.vision_args.get("distance_args").get("min_dist")
         self.highlight_max_dist = self.vision_args.get("distance_args").get("max_dist")
         self.highlight_color = tuple(int(c) for c in self.vision_args.get("distance_args").get("color"))
@@ -68,7 +74,7 @@ class Vision:
             h, w = frame.shape[:2]
             mid = w // 2
             if self.cam_matrix is not None and self.dist_coeffs is not None:
-                map1_x, map1_y, map2_x, map2_y, Q = self.generate_rectify_maps((mid, h), scale)
+                self.map1_x, self.map1_y, self.map2_x, self.map2_y, self.Q = self.generate_rectify_maps((mid, h), scale)
                 server_logger.get_logger().info("Rectification maps generated")
             else:
                 raise ValueError(f"cam_matrix is set to: {self.cam_matrix}, dist_coeffs is set to: {self.dist_coeffs}")
@@ -87,8 +93,8 @@ class Vision:
                         left_frame = cv2.resize(left_frame, (int(mid * scale), int(h * scale)))
                         right_frame = cv2.resize(right_frame, (int(mid * scale), int(h * scale)))
 
-                        left = cv2.remap(left_frame, map1_x, map1_y, cv2.INTER_LINEAR)
-                        right = cv2.remap(right_frame, map2_x, map2_y, cv2.INTER_LINEAR)
+                        left = cv2.remap(left_frame, self.map1_x, self.map1_y, cv2.INTER_LINEAR)
+                        right = cv2.remap(right_frame, self.map2_x, self.map2_y, cv2.INTER_LINEAR)
 
                         gray_left = cv2.cvtColor(left, cv2.COLOR_BGR2GRAY)
                         gray_right = cv2.cvtColor(right, cv2.COLOR_BGR2GRAY)
@@ -114,10 +120,8 @@ class Vision:
                                 (filtered_disp[valid_mask] - min_val) / (max_val - min_val) * 255).astype(
                             np.uint8)
 
-                        distance_map = disparity_to_distance(filtered_disp, Q)
-                        highlighted_frame = self.highlight_distance_range(left, distance_map,
-                                                                          self.highlight_min_dist,
-                                                                          self.highlight_max_dist)
+                        distance_map = self.disparity_to_distance(filtered_disp)
+                        highlighted_frame = self.highlight_distance_range(left, distance_map)
 
                         disp_colored = cv2.applyColorMap(disp_normalized.astype(np.uint8), cv2.COLORMAP_INFERNO)
 
@@ -197,32 +201,43 @@ class Vision:
 
         return map1_x, map1_y, map2_x, map2_y, Q
 
-    def highlight_distance_range(self, frame, distance_map, min_dist, max_dist):
-        result = frame.copy()
-        valid_pixels = ~np.isnan(distance_map)
+    def highlight_distance_range(self, frame, distance_map):
+        """
+        This function draws the colored outline on the image that tells us what is in range
+        :param frame: This is the visual color normal frame, 3D array
+        :param distance_map: This is the array that holds all of the distances 2D array
+        :return: A visual frame with the green color on it of where the close section is
+        """
+        valid_pixels = ~np.isnan(distance_map)  # Here we make sure they are all valid
 
-        range_mask = np.zeros_like(distance_map, dtype=np.uint8)
-        in_range = (distance_map >= min_dist) & (distance_map <= max_dist) & valid_pixels
-        range_mask[in_range] = 1
+        range_mask = np.zeros_like(distance_map, dtype=np.uint8)  # We create an empty matrix with the same size and stuff as the map
+        in_range = (distance_map >= self.highlight_min_dist) & (distance_map <= self.highlight_max_dist) & valid_pixels
+        range_mask[in_range] = 1  # We populate each value that meets the criteria of our distance with 1 so we know what is in range
 
-        kernel = np.ones((5, 5), np.uint8)
-        range_mask = cv2.dilate(range_mask, kernel, iterations=1)
-
-        # We find contours to filter out what we highlight
+        # We find contours to filter out what we highlight, contours basically act as a filter as it makes areas of congested regions
         contours, _ = cv2.findContours(range_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        highlight_mask = np.zeros_like(range_mask)  # This mask is the same dimensions as the range 0,1 mask
         for contour in contours:
-            if cv2.contourArea(contour) >= self.highlight_min_area:
-                cv2.drawContours(result, [contour], -1, self.highlight_color, 2)
-                cv2.fillPoly(result, [contour], self.highlight_color)
+            if cv2.contourArea(contour) >= self.highlight_min_area:  # If the contour is big enough to be considered valid
+                cv2.drawContours(highlight_mask, [contour], -1, self.highlight_color, 2)
+                cv2.fillPoly(highlight_mask, [contour], self.highlight_color)
 
-        return cv2.addWeighted(result, self.highlight_alpha, frame, 1 - self.highlight_alpha, 0)
+        highlight = np.zeros_like(frame)  # This is what we will display, we create a new matrix the same size as the orig frame
+        highlight[highlight_mask > 0] = self.highlight_color  # We populate valid values with our color we choose
+
+        return cv2.addWeighted(highlight, self.highlight_alpha, frame, 1 - self.highlight_alpha, 0)  # We overlay it onto the visual frame
 
 
-def disparity_to_distance(disparity, Q):
-    valid_mask = (disparity > 0.1)
+    def disparity_to_distance(self, disparity):
+        """
+        This is a pretty simple function we just do a simple calculation to get the distance for each value in the disparity array
+        :param disparity: The disparity map 2D
+        :return: The distance map, a 2D array with each value being the distance at that point
+        """
+        valid_mask = (disparity > 0.1)  # We check for valid areas
 
-    points_3d = cv2.reprojectImageTo3D(disparity, Q)
-    distance_map = points_3d[:, :, 2]
-    distance_map[~valid_mask] = np.nan
+        points_3d = cv2.reprojectImageTo3D(disparity, self.Q)  # Pretty self explanatory
+        distance_map = points_3d[:, :, 2]  # We get the Z value from the 3D matrix
+        distance_map[~valid_mask] = np.nan  # And we fill in the matrix with valid values
 
-    return distance_map
+        return distance_map
